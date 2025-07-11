9d91f76cfb06a886faa3f8917d2734eed90555ca
BananaJeanss <18456310+BananaJeanss@users.noreply.github.com>
2025-07-11 23:18:45 +0300

    fix: refactor uptime command to calculate uptime directly in execute function

--- src/commands/utility/uptime.ts
+++ src/commands/utility/uptime.ts
@@ -1,21 +1,18 @@
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 
-const formattedUptime = (() => {
-  const uptime = process.uptime();
-  const uptimeDays = Math.floor(uptime / 86400);
-  const uptimeHours = Math.floor((uptime % 86400) / 3600);
-  const uptimeMinutes = Math.floor((uptime % 3600) / 60);
-  const uptimeSeconds = Math.floor(uptime % 60);
-
-  return `${uptimeDays}d ${uptimeHours}h ${uptimeMinutes}m ${uptimeSeconds}s`;
-})();
-
-
 export default {
   data: new SlashCommandBuilder()
     .setName("uptime")
     .setDescription("Replies with the bot's uptime"),
   async execute(interaction: CommandInteraction) {
+    const uptime = process.uptime();
+    const uptimeDays = Math.floor(uptime / 86400);
+    const uptimeHours = Math.floor((uptime % 86400) / 3600);
+    const uptimeMinutes = Math.floor((uptime % 3600) / 60);
+    const uptimeSeconds = Math.floor(uptime % 60);
+
+    const formattedUptime = `${uptimeDays}d ${uptimeHours}h ${uptimeMinutes}m ${uptimeSeconds}s`;
+
     await interaction.reply({
       content: `Uptime: ${formattedUptime}`,
     });
