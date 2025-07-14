c6190667d228ce9f1a570cd785034aec1dcdd8b0
BananaJeanss <18456310+BananaJeanss@users.noreply.github.com>
2025-07-14 22:34:55 +0300

    feat: enhance environment variable validation, update express server port configuration, page styling improvements

--- src/events/envCheck.ts
+++ src/events/envCheck.ts
@@ -5,6 +5,7 @@ import { config } from 'dotenv';
 config();
 
 export function envCheck() {
+  let isWarning = false;
   const requiredEnvVars = [
     'DISCORD_BOT_TOKEN',
     'DISCORD_CLIENT_ID',
@@ -14,6 +15,24 @@ export function envCheck() {
         console.error('Missing required environment variables:', missingEnvVars.join(', '));
         process.exit(1);
         }
-    console.log('⚙️  envCheck Passed');
+    
+    // optional env vars
+    if (!process.env.RUN_MODE) {
+        console.warn('RUN_MODE is not set, defaulting to "dual"');
+        process.env.RUN_MODE = 'dual';
+        isWarning = true;
+    }
+
+    if (!process.env.EXPRESS_PORT) {
+        console.warn('EXPRESS_PORT is not set, defaulting to 3000');
+        process.env.EXPRESS_PORT = '3000';
+        isWarning = true;
+    }
+
+    if (isWarning) {
+        console.warn('⚙️⚠️  envCheck Passed with warnings');
+    } else {
+        console.log('⚙️  envCheck Passed');
+    }
     return true;
 }
