1287fb0511ec455462e512d18047944f7134e3c3
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-03-26 15:14:32 +0200

    i forgot to loop updateCount

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,5 @@
 import { Client, Events } from "discord.js";
 import { startRotatingStatus } from "./rotateStatus.js";
-import { ServerCount } from "../metrics.js";
 import { updateCount } from "./guildCount.js";
 
 export default {
@@ -8,8 +7,12 @@ export default {
   once: true,
   execute(client: Client) {
     console.log(`🍏 Ralseibot ready! Logged in as ${client.user?.tag}`);
+    
     // init prometheus member count
-	updateCount(client);
+    updateCount(client);
+    setInterval(() => updateCount(client), 15 * 60 * 1000);
+
+    // rotate statuses
     startRotatingStatus(client);
   },
 };
